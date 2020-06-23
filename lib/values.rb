b46eaa81070e4a98425466b91dae34b12d0a4ac4
Lesly Juarez <leslyj2@gmail.com>
2020-06-23 18:30:54 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -7,3 +7,9 @@ require 'pry'
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
 # }
+
+
+def get_the_values(groceries)
+  #code your solution here!
+  groceries.values.flatten
+end
